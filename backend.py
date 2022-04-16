85ed45884b1220d002194d703e8bfd2fc5f93c7e
Luka Jovanovic <lukaj2501@gmail.com>
2022-04-16 19:45:47 -0400

    began work on backend

--- backend.py
+++ backend.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 from pathlib import Path
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 @app.route("/")
 @app.route("/index/")
@@ -24,8 +25,21 @@ def about():
 	return render_template("about.html")
 
 
-@app.route("/login/")
+@app.route("/login/", methods=["POST", "GET"])
 def login():
+	if request.method == "POST":
+		username = request.form['usrnm']
+		password = request.form['pswrd']
+		session['user'] = [username, password]
+		return render_template("index.html")
+	else:
+		if "user" in session:
+			return redirect("index.html")
+		return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+	session.pop("user", None)
 	return render_template("login.html")
 
 
