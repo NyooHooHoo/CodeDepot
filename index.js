14d958a06772633fd5e649df93b05ce0e2e15914
Luka Jovanovic <lukaj2501@gmail.com>
2022-04-16 14:24:47 -0400

    loading courses with javascript

--- index.js
+++ index.js
@@ -62,7 +62,8 @@ let data =
   }
 }
 
-let filteredSearch = {}
+let filteredSearch = {};
+var courses = [];
 
 function search() {
 
@@ -85,7 +86,72 @@ function search() {
 
 class Course {
 	constructor(title, length, author, description, url, thumbnail, likes, dislikes) {
-    
+    this.title = title;
+    this.length = length;
+    this.author = author;
+    this.description = description;
+    this.url = url;
+    this.thumbnail = thumbnail;
+    this.likes = likes;
+    this.dislikes = dislikes;
+    this.score = (likes - dislikes).toString();
 	}
+
+  generateHTML() {
+    return `
+    <div class="course-item">
+      <img class="course-image" src="assets/thumbnails/${this.thumbnail}" alt="Thumbnail">
+      <div class="course-information">
+        <t class="course-title">${this.title}</t>
+        
+        <br>
+        Length: ${this.length}<br>
+        By:
+        <t class="author">${this.author}</t><br><br>
+        <t>${this.description}</t>
+        <br><br><br><br>
+      </div>
+      <div class="course-footer">
+        <img class="rating" src="assets/like_default.png" onclick="like()"/>
+        <t id="rating1" class="rating-value">${this.score}</t>
+        <img class="rating" src="assets/dislike_default.png" onclick="dislike()"/>
+
+        <p></p>
+
+        <a class="learn-now" href="${this.url}" target="_blank">LEARN</a>
+        <p></p>
+        <img class="favourite" src="assets/star_default.png" onclick="togglefavourite()"/>
+      
+      </div>
+    </div>
+    `;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  let courselist = document.getElementById("recommended-list");
+  let html = document.getElementById("html");
+
+  for (key in data) {
+    let title = data[key].title;
+    let length = data[key].length;
+    let author = data[key].author;
+    let description = data[key].description;
+    let url = data[key].url;
+    let thumbnail = data[key].thumbnail;
+    let likes = data[key].likes;
+    let dislikes = data[key].dislikes;
+
+    let course = new Course(title, length, author, description, url, thumbnail, likes, dislikes);
+    courses.push(course);
+  }
+
+  for (course of courses) {
+    courselist.innerHTML += course.generateHTML();
+  }
+  html.innerHTML += `<footer>
+    <p>Made by The Code Depot Team</p>
+    <a href="https://github.com/nelimalu/CodeDepot" target="_blank"><img id="github-link" src="assets/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png"></a>
+  </footer>`;
+});
 
