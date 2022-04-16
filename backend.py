548cbfa8790ab87c3a0da2584292eb532e2ec3e3
Song Wang <tom@iMac-2.local>
2022-04-16 18:26:41 -0400

    linked login button to page

--- backend.py
+++ backend.py
@@ -24,5 +24,10 @@ def about():
 	return render_template("about.html")
 
 
+@app.route("/login/")
+def login():
+	return render_template("login.html")
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
