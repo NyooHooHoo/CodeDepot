a8fe2af5cb14c8043d164f72351f53ddd0817a1d
Luka Jovanovic <lukaj2501@gmail.com>
2022-04-16 21:16:03 -0400

    created signup in backend

--- backend.py
+++ backend.py
@@ -1,10 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=1)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+	_id = db.Column("id", db.Integer, primary_key=True)
+	username = db.Column(db.String(100))
+	password = db.Column(db.String(100))
+	liked = db.Column(db.JSON)
+	disliked = db.Column(db.JSON)
+	favourited = db.Column(db.JSON)
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+
 
 @app.route("/")
 def home():
@@ -24,6 +42,28 @@ def favourites():
 @app.route("/about/")
 def about():
 	return render_template("about.html")
+
+
+@app.route("/signup/", methods=["POST", "GET"])
+def signup():
+	if request.method == "POST":
+		session.permanent = True
+		username = request.form['usrname']
+		password = request.form['psword']
+
+		found_user = users.query.filter_by(username=username).first()
+		if found_user:
+			# username already taken!
+			pass
+		else:
+			usr = users(username, password)
+			db.session.add(usr)
+			db.session.commit()
+
+		session['user'] = [username, password]
+		return redirect("/")
+	else:
+		return render_template("signup.html")
 
 
 @app.route("/login/", methods=["POST", "GET"])
@@ -46,4 +86,5 @@ def logout():
 
 
 if __name__ == "__main__":
+	db.create_all()
 	app.run(debug=True)
